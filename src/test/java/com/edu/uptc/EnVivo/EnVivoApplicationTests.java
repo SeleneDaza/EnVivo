28621e683dca1d93d1172e10c5dae15ff5b4931c
Valeriatm06 <valeria.tocarruncho@uptc.ecu.co>
2026-02-18 17:59:39 -0500

    spring boot structure

---
+++ src/test/java/com/edu/uptc/EnVivo/EnVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.EnVivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnVivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
