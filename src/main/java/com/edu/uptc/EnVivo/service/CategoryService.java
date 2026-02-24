27eadbee5de3fd6af318151e7c82d546fa28e17c
Valeriatm06 <valeria.tocarruncho@uptc.ecu.co>
2026-02-24 15:07:40 -0500

    service de  categoría

---
+++ src/main/java/com/edu/uptc/EnVivo/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.edu.uptc.EnVivo.service;
+
+import com.edu.uptc.EnVivo.dto.CreateCategoryDTO;
+import com.edu.uptc.EnVivo.entity.Category;
+import com.edu.uptc.EnVivo.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public Category createCategory(CreateCategoryDTO dto) {
+        Category category = new Category();
+        category.setName(dto.getName());
+        return categoryRepository.save(category);
+    }
+}
