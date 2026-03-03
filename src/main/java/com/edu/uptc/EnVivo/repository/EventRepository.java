b7ff9d6b566bfb715f54b9c65356e1d9f4d82288
Valeriatm06 <valeria.tocarruncho@uptc.ecu.co>
2026-03-03 18:58:35 -0500

    Resporte de eventos listo

--- src/main/java/com/edu/uptc/EnVivo/repository/EventRepository.java
+++ src/main/java/com/edu/uptc/EnVivo/repository/EventRepository.java
@@ -4,11 +4,15 @@ import com.edu.uptc.EnVivo.entity.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     Page<Event> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    @Query("SELECT e FROM Event e LEFT JOIN e.favoritedByUsers u GROUP BY e ORDER BY COUNT(u) DESC")
+    List<Event> findTop10ByFavoritesCount(Pageable pageable);
 
 }
